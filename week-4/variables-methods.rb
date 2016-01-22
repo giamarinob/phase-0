c59ab6e11f2208d488ab31ff246b5b860607743d
[Ben] <giamarinob@gmail.com>
2016-01-22 21:18:28 +0000

    4.3 Variables and Methods

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -11,3 +11,45 @@ num = gets.chomp
 num = num.to_i
 num += 1
 puts "Your number is pretty cool, but #{num} is bigger and better than yours!"
+
+=begin
+4.3.1 Return a Formatted Address
+
+https://github.com/giamarinob/phase-0/tree/master/week-4/address
+================================================================
+4.3.2 Defining Math Methods
+
+https://github.com/giamarinob/phase-0/tree/master/week-4/math
+
+================================================================
+
+How do you define a local variable?
+  A local variable is defined with an assignment statement. my_variable = "whatever"
+  A local variable can have the scope of a particular method or function or it
+  can have the scope of an entire document. 
+  
+How do you define a method?
+  A method must include, at the very least the key word "def" followed by the 
+  method name. It must also be closed with the "end" keyword. It could contain code
+  inside, but it isn't required to get the method to compile. The same goes for
+  parameters. It can have them, but it doesn't need them.
+  
+What is the difference between a local variable and a method?
+  A local variable is a single object. They dont't really do anything when called
+  upon other than hold some value. A method, when called on, performs some function
+  when called. A method can perform its function on a local variable. THe local variable
+  can be contained within a method. 
+  
+How do you run a ruby program from the command line?
+  In c9 I simply type ruby filename.rb
+  
+How do you run an RSpec file from the command line?
+  In c9 I simply type rspec filename.rb
+  
+What was confusing about this material? What made sense?
+  I did not find anything confusing about this challenge. The only thing I had problems
+  with were little things like having the method return a string instead of putting it out
+  to the screen, or forgetting to put def in front of my method name when creating it
+  which made the rspec error interesting to say the least. 
+=end
+
